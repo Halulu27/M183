0f1021f16f0f04b110e491c4a7018f60c0e712a2
Halulu27 <odermatt.simon@gmail.com>
2018-09-17 13:50:07 +0200

    fdsds

--- Block4_SMSOTP/Block4_SMSOTP/Controllers/HomeController.cs
+++ Block4_SMSOTP/Block4_SMSOTP/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,19 +31,49 @@ namespace Block4_SMSOTP.Controllers
 
             if (username == "test" && password == "test")
             {
-                var request = (HttpWeRequest) WebRequest.Creat("https://rest.nexmo.com/sms/json");
-                var secret = "fbkJ6jBjYGS2IkkT";
-                var postData = "api_key = 7e42b8cd";
-                postData += "&"
+                var request = (HttpWebRequest) WebRequest.Create("https://rest.nexmo.com/sms/json");
+                var secret = "Test_SECRET";
+                var postData = "api_key=7e42b8cd";
+                postData += "&api_secret=fbkJ6jBjYGS2IkkT";
+                postData += "&to=41794517073";
+                postData += "&from=NEXMO";
+                postData += "text=Hello from Nexmo";
+                var data = Encoding.ASCII.GetBytes((postData));
 
-                curl - X POST https://rest.nexmo.com/sms/json \
-                -d api_key = 7e42b8cd \
-                -d api_secret = fbkJ6jBjYGS2IkkT \
-                -d to = 41794517073 \
-                -d from = "NEXMO" \
-                -d text = "Hello from Nexmo"
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded: application/json";
+                request.ContentLength = data.Length;
+                request.KeepAlive = true;
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+                var response = (HttpWebResponse) request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                ViewBag.Message = responseString;
+            }
+            else
+            {
+                ViewBag.Message = "Wrong Credentials";
             }
             return View();
+        }
+
+        [HttpPost]
+        public void TokenLogin()
+        {
+            var token = Request["token"];
+            if (token == "Test_SCRET")
+            {
+
+            }
+            else
+            {
+                
+            }
         }
 
         public ActionResult Contact()
