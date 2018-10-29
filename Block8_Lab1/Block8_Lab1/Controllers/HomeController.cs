06840890eb6f13976557481300f5886efb6a4006
Simon Odermatt <odermatt.simon@gmail.com>
2018-10-29 14:39:10 +0100

    commit

---
+++ Block8_Lab1/Block8_Lab1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Block8_Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            var username = Request["Username"];
+            var password = Request["Password"];
+            if (username == "test" && password == "test")
+            {
+                FormsAuthentication.SetAuthCookie("test", false);
+                return RedirectToAction("Success");
+            }
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
