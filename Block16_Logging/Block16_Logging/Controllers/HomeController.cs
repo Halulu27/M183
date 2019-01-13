21d750f4ab75438bf4624005615d3102eaeec48b
Simon Odermatt <odermatt.simon@gmail.com>
2019-01-13 18:29:59 +0100

    completed Logging Block 16

--- Block16_Logging/Block16_Logging/Controllers/HomeController.cs
+++ Block16_Logging/Block16_Logging/Controllers/HomeController.cs
@@ -4,38 +4,16 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Block16_Logging.Models;
 
 namespace Block16_Logging.Controllers
 {
     public class HomeController : Controller
     {
+        private static string dbConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gibz\Documents\logging_instrusion_detection.mdf;Integrated Security=True;Connect Timeout=30";
+
         public ActionResult Index()
         {
-            //var con = new SqlConnection
-            //{
-            //    ConnectionString =
-            //        @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gibz\Documents\logging_instrusion_detection.mdf;Integrated Security=True;Connect Timeout=30"
-            //};
-
-            //SqlCommand cmd = new SqlCommand();
-            //SqlDataReader reader;
-
-            //cmd.CommandText = "SELECT [Id], [Username], [Password] FROM [dbo].[User];";
-            //cmd.Connection = con;
-
-            //reader = cmd.ExecuteReader();
-            //if (reader.HasRows)
-            //{
-            //    while (reader.Read())
-            //    {
-            //        Console.WriteLine($"{reader.GetInt32(0)}\t{reader.GetString(1)}");
-            //    }
-            //}
-            //{
-            //    Console.WriteLine("No rows found.");
-            //}
-
-
             return View();
         }
 
@@ -61,12 +39,11 @@ namespace Block16_Logging.Controllers
 
             var ip = Request.ServerVariables["REMOTE_ADDR"];
             var platform = Request.Browser.Platform;
-            var browser = Request.UserAgent;
+            var additionalInfo = Request.UserAgent;
 
             var con = new SqlConnection
             {
-                ConnectionString =
-                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gibz\Documents\logging_instrusion_detection.mdf;Integrated Security=True;Connect Timeout=30"
+                ConnectionString = dbConnectionString
             };
 
             var cmd = new SqlCommand
@@ -100,7 +77,7 @@ namespace Block16_Logging.Controllers
                             con.Open();
                             var sqlInsertCommand = new SqlCommand
                             {
-                                CommandText = $"INSERT INTO [dbo].[UserLog] (UserId, IP, Action, Result, CreatedOn, Browser, AdditionalInformation) VALUES('{userId}', '{ip}', 'login', 'success', GETDATE(), '{platform}', 'other browser')",
+                                CommandText = $"INSERT INTO [dbo].[UserLog] (UserId, IP, Action, Result, CreatedOn, Browser, AdditionalInformation) VALUES('{userId}', '{ip}', 'login', 'success', GETDATE(), '{platform}', '{additionalInfo}')",
                                 Connection = con
                             };
                             sqlInsertCommand.ExecuteReader();
@@ -124,7 +101,7 @@ namespace Block16_Logging.Controllers
                     con.Open();
                     var sqlCommand = new SqlCommand
                     {
-                        CommandText = $"SELECT [Id] FROM [dbo].[User] WHEE [Username] = '{username}';",
+                        CommandText = $"SELECT [Id] FROM [dbo].[User] WHERE [Username] = '{username}';",
                         Connection = con
                     };
                     using (var sqlReader = sqlCommand.ExecuteReader())
@@ -142,14 +119,51 @@ namespace Block16_Logging.Controllers
 
                             var sqlCountCommand = new SqlCommand
                             {
-                                CommandText = $"SELECT COUNT(Id) FROM [dbo].[UserLog] WHERE [UserId] = '{userId}'AND [Result] = 'failed' AND CAST(CreatedOn AS date) = '{System.DateTime.Now.ToShortDateString().Substring(0, 10)}'",
+                                CommandText = $"SELECT COUNT(Id) FROM [dbo].[UserLog] WHERE [UserId] = '{userId}' AND [Result] = 'failed' AND CAST(CreatedOn AS date) = '{DateTime.Now.ToShortDateString().Substring(0, 10)}'",
                                 Connection = con
                             };
+                            using (var failedLoginCount = sqlCountCommand.ExecuteReader())
+                            {
+                                var attempts = 0;
+                                if (failedLoginCount.HasRows)
+                                {
+                                    while (failedLoginCount.Read())
+                                    {
+                                        attempts = failedLoginCount.GetInt32(0);
+                                        break;
+                                    }
+                                }
+                                if (attempts >= 5 || password.Length < 4 || password.Length > 20)
+                                {
+                                    // block user
+                                }
+                            }
+
+                            con.Close();
+                            con.Open();
+
+                            var sqlLogCmd = new SqlCommand
+                            {
+                                CommandText = $"INSERT INTO [dbo].[UserLog] (UserId, IP, Action, Result, CreatedOn, Browser) VALUES('{userId}', '{ip}', 'login', 'failed', GETDATE(), '{platform}')",
+                                Connection = con
+                            };
+                            sqlLogCmd.ExecuteReader();
+
+                            ViewBag.Message = "No user found";
 
                         }
                         else
                         {
-                            
+                            con.Close();
+                            con.Open();
+
+                            var sqlLogCmd = new SqlCommand
+                            {
+                                CommandText = $"INSERT INTO [dbo].[UserLog] (UserId, IP, Action, Result, CreatedOn, AdditionalInformation, Browser) VALUES(0, '{ip}', 'login', 'failed', GETDATE(), 'No User Found', '{platform}')",
+                                Connection = con
+                            };
+                            sqlLogCmd.ExecuteReader();
+                            ViewBag.Message = "No user found";
                         }
                     }
                 }
@@ -157,6 +171,38 @@ namespace Block16_Logging.Controllers
             con.Close();
 
             return RedirectToAction("Logs", "Home");
+        }
+
+        public ActionResult Logs()
+        {
+            var con = new SqlConnection
+            {
+                ConnectionString = dbConnectionString
+            };
+
+            var sqlCredentialsCmd = new SqlCommand
+            {
+                CommandText = "SELECT * FROM [dbo].[UerLog] ul JOIN [dbo].[User] u ON ul.UserId = u.Id ORDER BY ul.CreatedOn DESC;",
+                Connection = con
+            };
+            con.Open();
+            using (var sqlReader = sqlCredentialsCmd.ExecuteReader())
+            {
+                var viewModel = new List<HomeControllerViewModel>();
+                if (sqlReader.HasRows)
+                {
+                    while (sqlReader.Read())
+                    {
+                        viewModel.Add(new HomeControllerViewModel
+                        {
+                            UserId = sqlReader.GetValue(10).ToString(),
+                            LogId = sqlReader.GetValue(0).ToString(),
+                            LogCreatedOn = sqlReader.GetValue(7).ToString()
+                        });
+                    }
+                }
+                return View(viewModel);
+            }
         }
     }
 }
---
+++ Block16_Logging/Block16_Logging/Models/HomeControllerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Block16_Logging.Models
+{
+    public class HomeControllerViewModel
+    {
+        public string UserId { get; set; }
+        public string LogId { get; set; }
+        public string LogCreatedOn { get; set; }
+    }
+}
